3d7f2b2396eff6349bb9a9054a648e33a3edcee8
gunesay <camdalkiran@gmail.com>
2023-08-04 22:28:51 +0300

    apply for a job project has finished with successfully

--- src/test/java/com/triple/runner/CukesRunner.java
+++ src/test/java/com/triple/runner/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features ="src/test/resources/features" ,
         glue = "com/triple/stepDefs",
         dryRun = false,
-        tags = ""
+        tags = "@apply"
 )
 public class CukesRunner {
 }
